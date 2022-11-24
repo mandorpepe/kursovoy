65abc82df3d5b484a5fa4ce42bfa59c86fd292a6
Артем Андреевич Бидуля <19056@irkat.ru>
2022-11-24 17:15:14 +0800

    удаля

--- kursovoy/adminScreen.Designer.cs
+++ kursovoy/adminScreen.Designer.cs
@@ -32,6 +32,13 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,13 +69,13 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(136, 55);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Зарегестрировать нового работника";
+            this.button2.Text = "Добавить";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 364);
+            this.button3.Location = new System.Drawing.Point(12, 399);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
@@ -76,11 +83,88 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(142, 261);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 55);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Клиенты";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(294, 261);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 55);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Отель";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(445, 261);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(136, 55);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Должность";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(217, 322);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 55);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Путёвка";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(379, 322);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(136, 55);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Работники";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(656, 322);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(136, 55);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Изменить";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(656, 383);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(136, 55);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Удалить";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
             // adminScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(825, 450);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
@@ -99,5 +183,12 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
--- kursovoy/adminScreen.cs
+++ kursovoy/adminScreen.cs
@@ -9,13 +9,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Azure;
+using Microsoft.Office.Interop.Excel;
 using MySql.Data.MySqlClient;
-
+using MySqlX.XDevAPI.Relational;
+using DataTable = System.Data.DataTable;
 
 namespace kursovoy
 {
     public partial class adminScreen : Form
     {
+        String seltab;
         //Подключение файла с подключением к бд
         DB db = new DB();
         public adminScreen()
@@ -77,6 +80,128 @@ namespace kursovoy
             Авторизация auth = new Авторизация();
             auth.Show();
             Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = db.getConnection();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "Select * from clients";
+            MySqlDataAdapter sqlDataAdap = new MySqlDataAdapter(command);
+            //Заполнение грида
+            DataTable dtRecord = new DataTable();
+            sqlDataAdap.Fill(dtRecord);
+            dataGridView1.DataSource = dtRecord;
+            seltab = "Клиенты";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = db.getConnection();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "Select * from hotel";
+            MySqlDataAdapter sqlDataAdap = new MySqlDataAdapter(command);
+            //Заполнение грида
+            DataTable dtRecord = new DataTable();
+            sqlDataAdap.Fill(dtRecord);
+            dataGridView1.DataSource = dtRecord;
+            seltab = "Отель";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = db.getConnection();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "Select * from position";
+            MySqlDataAdapter sqlDataAdap = new MySqlDataAdapter(command);
+            //Заполнение грида
+            DataTable dtRecord = new DataTable();
+            sqlDataAdap.Fill(dtRecord);
+            dataGridView1.DataSource = dtRecord;
+            seltab = "Должность";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = db.getConnection();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "Select * from trip";
+            MySqlDataAdapter sqlDataAdap = new MySqlDataAdapter(command);
+            //Заполнение грида
+            DataTable dtRecord = new DataTable();
+            sqlDataAdap.Fill(dtRecord);
+            dataGridView1.DataSource = dtRecord;
+            seltab = "Путёвка";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = db.getConnection();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "Select * from worker";
+            MySqlDataAdapter sqlDataAdap = new MySqlDataAdapter(command);
+            //Заполнение грида
+            DataTable dtRecord = new DataTable();
+            sqlDataAdap.Fill(dtRecord);
+            dataGridView1.DataSource = dtRecord;
+            seltab = "Работник";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            DataTable table = new DataTable();
+            MySqlCommand command = new MySqlCommand();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            command.Connection = db.getConnection();
+            command.CommandType = CommandType.Text;
+            int rowindex = dataGridView1.CurrentCell.RowIndex;
+            int columnindex = dataGridView1.CurrentCell.ColumnIndex;
+            int need = (int)dataGridView1.Rows[rowindex].Cells[columnindex].Value;
+            command.Parameters.Add("@id", MySqlDbType.Int32).Value = need;
+            switch (seltab)
+            {
+                case "Клиенты":
+                    command.CommandText = "delete from clients where id_clients = @id";
+                    adapter.SelectCommand = command;
+                    adapter.Fill(table);
+                    MessageBox.Show("Удалено успешно");
+                    break;
+                case "Отель":
+                    command.CommandText = "delete from hotel where id_hotel = @id";
+                    adapter.SelectCommand = command;
+                    adapter.Fill(table);
+                    MessageBox.Show("Удалено успешно");
+                    break;
+                case "Должность":
+                    command.CommandText = "delete from position where id_position = @id";
+                    adapter.SelectCommand = command;
+                    adapter.Fill(table);
+                    MessageBox.Show("Удалено успешно");
+                    break;
+                case "Путёвка":
+                    command.CommandText = "delete from trip where id_book = @id";
+                    adapter.SelectCommand = command;
+                    adapter.Fill(table);
+                    MessageBox.Show("Удалено успешно");
+                    break;
+                case "Работник":
+                    command.CommandText = "delete from worker where id_worker = @id";
+                    adapter.SelectCommand = command;
+                    adapter.Fill(table);
+                    MessageBox.Show("Удалено успешно");
+                    break;
+            }
         }
     }
 }
